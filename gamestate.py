23b4a15ec715fcfe4a8e53e59a05dea58d112acb
itsspher <43953524+itsspher@users.noreply.github.com>
2018-11-22 15:15:03 +0800

    pre-alpha v1.2
    - removed some comments
    + added a module to handle the game's engine

---
+++ gamestate.py
@@ -0,0 +1,60 @@
+import pyglet
+
+class GameState:
+	def __init__(self, saveFile = None):
+		self.saveFile = savefile if saveFile is not None else 'init'
+		self.dataFileCount = int()
+		self.cumulativeDataFileCount = int()
+		self.cashCount = int()
+		self.bitcoinCount = int()
+		self.collectorCount = int()
+		self.databaseCount = int()
+		self.laundromatCount = int()
+		self.attributeList = [self.dataFileCount, self.cumulativeDataFileCount, self.cashCount, self.bitcoinCount,
+self.collectorCount, self.databaseCount, self.laundromatCount]
+	
+	def loadSaveData(self):
+		save = open(self.saveFile + '.txt', 'r')
+		saveData = save.readlines()
+		saveData = [x.strip() for x in saveData]
+		for i in range(len(saveData)):
+			self.attributeList[i] = saveData[i]
+		
+	def collectData(self):
+		self.dataFileCount += 1
+		
+	def cashInData(self):
+		if self.dataFileCount < 10:
+			return
+		self.dataFileCount -= 10
+		n = 0
+		success_chance = round(random.uniform(0.45, 0.55)) + database_modifier
+		for _ in range(10):
+			c = round(random.random())
+			if c <= success_chance:
+				n += 2500
+		self.cashCount += n
+	
+	def hireCollector(self):
+		if self.cashCount < 100:
+			return
+		self.collectorCount += 1
+		self.cashCount -= 100
+	
+	def hireLaudromat(self):
+		if self.cashCount < 50:
+			return
+		self.cashCount -= 50
+		self.laundromatCount += 1
+	
+	def buyBitcoin(self):
+		if self.cashCount < 5000:
+			return
+		self.cashCount -= 5000
+		self.bitcoinCount += 1
+	
+	def sellBitcoin(self):
+		if bitcoinCount < 1:
+			return
+		self.bitcoinCount -= 1
+		self.cashCount += 5000
--- gui.py
+++ gui.py
@@ -5,15 +5,11 @@ pyglet.resource.reindex()
 
 #please remember that 0,0 is in the lower left for pyglet
 
-'''
-class Button:
-	
-	def __init__(x, y, width, height, label):
-'''
 time = gametime.gameTimeData(1, 1, 1, 1)
 timeLabel = "Day: {}\n Week: {}\n Month: {}\n Year: {}".format(time.day, 
 time.week, time.month, time.year)
 
+startScreen = pyglet.window.Window(width = 400, height = 400)
 window = pyglet.window.Window(width = 1200, height = 900)	
 timeDisplay = pyglet.text.Label(
 timeLabel,
@@ -40,6 +36,7 @@ class Button:
 		self.name = name
 		self.neutral_image = pyglet.resource.image(self.name + '.png')
 		self.pressed_image = pyglet.resource.image(self.name + 'P' + '.png')
+		#self.hovered_image = pyglet.resource.image(self.name + 'H' + '.png')
 		self.button_sprite = pyglet.sprite.Sprite(self.neutral_image, self.x, self.y)
 		self.width = self.neutral_image.width
 		self.height = self.neutral_image.height
@@ -56,6 +53,19 @@ class Button:
 		self.button_sprite.image = self.neutral_image
 		self.button_sprite.draw()
 		print('{} button released'.format(self.name))
+	
+	def hovered(self):
+		"""
+		self.button_sprite.image = self.pressed_image
+		self.button_sprite.draw()
+		"""
+		pass
+	
+	def unhovered(self):
+		"""
+		self.button_sprite.image = self.neutral_image
+		self.button_sprite.draw()"""
+		pass
 	
 	def cursorOnButton(self, mouseX, mouseY):
 		if self.x <= mouseX <= self.x + self.width and self.y <= mouseY <= self.y + self.height:
@@ -76,6 +86,14 @@ def on_mouse_release(x, y, button, modifiers):
 	for button in buttonList:
 		if button.cursorOnButton(x, y):
 			button.unpressed()
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+	for button in buttonList:
+		if button.cursorOnButton(x, y):
+			button.hovered()
+		else:
+			button.unhovered()
 		
 @window.event
 def on_draw():
