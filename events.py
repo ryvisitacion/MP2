9143261997a6e17062644be7e26e541094767828
ryvisitacion <45007799+ryvisitacion@users.noreply.github.com>
2018-11-24 23:09:33 +0800

    Update events.py

--- events.py
+++ events.py
@@ -19,3 +19,29 @@ def dataLeakEvent(database_count, data_files_count, bitcoin_count):
 		t += round((.025 * r), 2)
 	return {'new_data_files':(data_files_count + n),
 	'new_bitcoin_count':(bitcoin_count - t)}
+
+def arrestEvent(laundromat_count):
+	arrest_chance = random.random(0, 1)
+	laundromat_modifier = 0
+	for i in range(laundromat_count):
+		laundromat_modifier += 0.5 * (1/1.25**(i-1))
+
+	cash_modifier = 0
+	excess = cash - 6000
+	if excess > 0:
+		x = excess//500
+		for i in range(x):
+			cash_modifier += 0.04
+
+	day_modifier = (day - 2) * 0.05
+
+	if arrest_chance <= (0.1 + laundromat_modifier + cash_modifier + day_modifier):
+		gameOverScreen()
+
+def irsVulnerability(collector_count):
+	irsVulnerabilityChance = random.random(0, 1)
+	if irsVulnerabilityChance <= 0.05:
+		x = 30 #number of days it takes to convert data files to cash
+		for _ in range(collector_count):
+			x -= round(5 * random.uniform(0.45, 0.55))
+		return x
